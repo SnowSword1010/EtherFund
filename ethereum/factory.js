068909a1933eecc5ecc2059cec327dca2933d27a
Divyanshu Gupta <gupta.divyanshuu@gmail.com>
2022-03-17 00:01:39 +0530

    deployed on rinkeby network

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,9 +1,9 @@
-import web3 from './web3';
-import CampaignFactory from './build/CampaignFactory.json';
+import web3 from "./web3";
+import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xB07Ff2bB7a4905FCCB65305F8F877fE5F0b44d8b'
+  "0x9a6c3C879feE9A5214faB41D5C327EF58e6e5db4"
 );
 
 export default instance;
